02d034176189094a0e8f1eeef13b494cb86f7427
RB4F505_032\Administrator <chanchai.chan@bumail.net>
2017-04-22 16:48:25 +0700

    ข้อที่ 8

---
+++ Assets/DestoryByconton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryByconton : MonoBehaviour {
+
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Boundary")
+		{
+			return;
+		}
+
+		Destroy (gameObject);
+}
+}
